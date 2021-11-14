8bbcb8989f19d8b98d4f908db9e582ddd97ec353
MrEmin <emuhammet.ozdemir@gmail.com>
2021-11-14 12:54:54 +0300

    Repl konusu

---
+++ intro/test.js
@@ -0,0 +1,10 @@
+console.log("Önemli bir bildirim Test.js");
+// terminale node yazıp enter'a basarak repl ortamına girmiş oluruz. Bir diğer ismide shell'dir.
+
+// console.clear() fonksiyonu repl ekranını temizler. 
+
+// Repl'ı google'daki console ortamı gibi düşünebiliriz. Ama node daha çok gelişmiştir.
+
+// .load test.js yazarak repl'a bu dosyayı çağırmış oluruz.
+
+// repl bazı test çalışmalarında kullanılabilir. Genelde çalışmalarımızda çok fazla dosya olduğundan repl'ı kullanmayız.
